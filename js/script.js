01cd66994687a63e15b2c36471083e85d31d6017
marcopolello <marco-pol@hotmail.it>
2020-12-09 19:47:56 +0100

    dati navbar top in js step 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
+    orario:{
+      stile: 'font-size:10px; display:inline-block;',
+      worktime: 'Mon-Sat 9:00-18:00',
+      icona: 'fas fa-clock'
+    },
     contatti: [
       {
         icona: 'fas fa-phone',
+        stile: 'font-size:10px; display:inline-block;',
         indirizzo: '+1(306)1234-5678',
         url: 'https://contacts.google.com/?hl=IT'
       },
       {
         icona: 'fas fa-envelope',
+        stile: 'font-size:10px; display:inline-block;',
         indirizzo: 'hello@example.com',
         url: 'https://contacts.google.com/?hl=IT'
       },
@@ -16,17 +23,17 @@ var app = new Vue({
     contattiSocial: [
       {
         icona: 'fab fa-facebook-f',
-        stile: 'font-size:10px; margin: 0px 5px',
+        stile: 'font-size:10px; margin: 0px 5px;',
         url: 'https://www.facebook.com/'
       },
       {
         icona: 'fab fa-twitter',
-        stile: 'font-size:10px; margin: 0px 5px',
+        stile: 'font-size:10px; margin: 0px 5px;',
         url: 'https://twitter.com/'
       },
       {
         icona: 'fab fa-linkedin-in',
-        stile: 'font-size:10px; margin: 0px 5px',
+        stile: 'font-size:10px; margin: 0px 5px;',
         url: 'https://www.linkedin.com/'
       },
     ]
